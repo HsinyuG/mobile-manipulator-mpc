26bf04e0809e608f6402d90688993f7e66b967a4
Xinyu Gao <x.gao-14@student.tudelft.nl>
2023-12-12 20:04:26 +0100

    code structure, not debugged

---
+++ controllers/mpc_demo.py
@@ -0,0 +1,96 @@
+import casadi as ca
+import matplotlib.pyplot as plt
+import numpy as np
+
+class MPC:
+    def __init__(self,
+        robot, 
+        N = 10, 
+        Q = np.diag([1., 0.0]), 
+        R = np.diag([0.1]), 
+        vlim=(-1, 1), 
+        alim=(-5, 5)):
+
+        self.Q = Q
+        self.R = R
+        self.dt = robot.dt
+        self.N = N
+        self.vlim = vlim
+        self.alim = alim
+        # System dynamics, which is a kinematic model
+        self.f_dynamics = robot.f_kinematics
+
+        self.reset()
+
+    def reset(self):
+        # Define optimization variables
+        self.opti = ca.Opti()
+
+        '''
+        reason for the implementation that X is Nx2 not 2xN: 
+        given p is a vector of same dim of x
+        if we use Nx2, then matrix mult = X @ p; and elementwise mult = X * p, 
+        of we use 2xN, then matrix mult = (X.T @ p).T; and elementwise mult = (X.T * p).T
+        '''
+        self.X = self.opti.variable(self.N+1, 2)    # states
+        self.U = self.opti.variable(self.N, 1)      # inputs
+
+        self.X_init = self.opti.variable(2)
+
+        self.X_ref = self.opti.parameter(self.N+1, 2)
+        self.U_ref = self.opti.parameter(self.N, 1)
+
+        self.X_guess = None
+        self.U_guess = None
+        
+        cost = 0
+        # Define constraints and cost
+        for k in range(self.N):
+            self.opti.subject_to(self.X[k+1, :] == self.f_dynamics(self.X[k, :], self.U[k, :]))
+            state_error = self.X[k, :] - self.X_ref[k, :]
+            control_error = self.U[k, :] - self.U_ref[k, :]
+            cost += ca.mtimes([state_error, self.Q, state_error.T]) \
+                                + ca.mtimes([control_error, self.R, control_error.T])
+            self.opti.subject_to(opti.bounded(self.alim[0], self.U[k, :], self.alim[1])) # acc constraint
+            self.opti.subject_to(opti.bounded(self.vlim[0], self.X[k, 1], self.vlim[1])) # vel constraint
+
+        self.opti.minimize(cost)
+
+        # Initial state as constraints
+        self.opti.subject_to(self.X[0, :] == self.X_init)
+
+        # Set solver options
+        # opts = {'ipopt.print_level': 0, 'print_time': 0, 'ipopt.tol': 1e-3}
+        opts_setting = {'ipopt.max_iter':2000,
+                        'ipopt.print_level':0,
+                        'print_time':0,
+                        'ipopt.acceptable_tol':1e-8,
+                        'ipopt.acceptable_obj_change_tol':1e-6}
+        self.opti.solver('ipopt', opts_setting)
+
+    def solve(self, x_init, traj_ref, u_ref):
+
+        # Set initial guess for the optimization problem
+        if self.X_guess is None:
+            self.X_guess = np.ones((self.N+1, 2) * x_init)
+
+        if self.U_guess is None:
+            self.U_guess = np.zeros((self.N, 1))
+        
+        self.opti.set_initial(X, self.X_guess)
+        self.opti.set_initial(U, self.U_guess)
+
+        self.opti.set_value(self.X_ref, self.traj_ref)
+        self.opti.set_value(self.U_ref, self.desired_acc)
+        
+        self.opti.set_value(self.X_init, x_init)
+
+        sol = self.opti.solve()
+        
+        ## obtain the initial guess of solutions of the next optimization problem
+        self.X_guess = sol.value(self.X)
+        self.U_guess = sol.value(self.U)
+        return self.U_guess[0, :]
+        
+
+
---
+++ robot_models/robot_demo.py
@@ -0,0 +1,12 @@
+import casadi as ca
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Robot:
+    def __init__(self, dt):
+        self.dt = dt
+        # self.model_params = whatever, not used in this demo
+
+    def f_kinematics(self, x, u):
+        # TODO: assert x.type == u.type == casadi variable
+        return ca.vertcat(x[0] + self.dt * x[1], x[1] + self.dt * u) # x[0] is position, x[1] is velocity, u is acceleration
