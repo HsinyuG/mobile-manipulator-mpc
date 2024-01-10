0d8f3367cb0d18ed6ffd1718a8aacc46783180d5
Xinyu Gao <x.gao-14@student.tudelft.nl>
2024-01-11 00:28:24 +0100

    combine state space, need to solve no accurate reach

---
+++ demo_wholebody_qref.py
@@ -0,0 +1,29 @@
+import casadi as ca
+import matplotlib.pyplot as plt
+import numpy as np
+
+from interface_wholebody_qref import Interface
+from controllers.mpc_wholebody_qref import MPCWholeBody
+from robot_models.mobile_manipulator import MobileManipulator
+from robot_models.obstacles import Obstacles
+
+dt = 0.1
+N = 50 
+t_move = 5 
+t_manipulate = 2
+x_start = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0])     
+# global_pose_target = np.array([5+0.6/ca.sqrt(2), 5+0.6/ca.sqrt(2), 0.606+0.333+0.5, ca.pi/4]) 
+global_pose_target = np.array([4.5+0.6/ca.sqrt(2), 4.5+0.6/ca.sqrt(2), 0.606+0.333+0.5, -ca.pi]) 
+
+obstacle_list = [
+    Obstacles(2.5, 3.0, 0.6),
+    Obstacles(2.5, 1.0, 0.6),
+    Obstacles(5+0.6/ca.sqrt(2), 5+0.6/ca.sqrt(2), 0.1)
+]
+
+mobile_manipulator = MobileManipulator(dt)
+mpc_controller = MPCWholeBody(mobile_manipulator, obstacle_list, N=N)
+world = Interface(dt, t_move, t_manipulate, x_start, global_pose_target, mpc_controller, physical_sim=True)
+
+world.run()
+world.plot3D()
